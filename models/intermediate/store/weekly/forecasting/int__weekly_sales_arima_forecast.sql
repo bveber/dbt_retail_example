3281c82d401b7a8779ceb2b57a14a4ae73eb7f09
Brandon Veber <bveber@phdata.io>
2023-03-27 21:05:34 +0000

    restructure project and disable the bigqueryml models for cost savings

--- models/intermediate/store/weekly/forecasting/int__weekly_sales_arima_forecast.sql
+++ models/intermediate/store/weekly/forecasting/int__weekly_sales_arima_forecast.sql
@@ -3,10 +3,19 @@ WITH forecasts AS (
 SELECT 
     s.store_name,
     c.category_name,
-    p.*,
+    p.store_number,
+    p.category,
+    p.forecast_timestamp,
+    p.forecast_value,
+    p.standard_error,
+    p.confidence_level,
+    p.prediction_interval_lower_bound,
+    p.prediction_interval_upper_bound,
+    p.confidence_interval_lower_bound,
+    p.confidence_interval_upper_bound,
     CASE WHEN p.forecast_value < 0 THEN 0 ELSE p.forecast_value END AS forecast_value_floor,
     DATE_TRUNC(CURRENT_DATE(), MONTH) AS forecast_generation_date
-FROM {{ iowa_liquor_sales.my_forecast(ref('int__weekly_sales_arima_model')) }} p
+FROM {{ iowa_liquor_sales.my_forecast(ref('int__weekly_sales_arima_model'), '12') }} p
 JOIN {{ ref('int__category_profile') }} c
     ON c.category = p.category
 JOIN {{ ref('int__store_profile') }} s
--- models/intermediate/store/weekly/processing/int__weekly_category_sales_filled_missing_dates.sql
+++ models/intermediate/store/weekly/processing/int__weekly_category_sales_filled_missing_dates.sql
@@ -13,11 +13,8 @@ SELECT distinct
     s.store_name,
     s.category,
     s.category_name,
-    m AS sales_week
-FROM {{ ref('int__weekly_category_sales') }} s,
-UNNEST(
-    GENERATE_DATE_ARRAY((SELECT min_date FROM min_max_dates), (SELECT max_date FROM min_max_dates), INTERVAL 1 WEEK)
-) m
+    s.sales_week
+FROM {{ ref('int__weekly_category_sales') }} s
 
 ),
 filled AS (
