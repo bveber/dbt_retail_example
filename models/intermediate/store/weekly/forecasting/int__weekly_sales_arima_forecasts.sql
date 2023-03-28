c0a63ddde934318f10242f42ba5f7e1d5e14b47a
Brandon Veber <bveber@phdata.io>
2023-03-28 00:10:25 +0000

    Finished data docs. Fixed naming

--- models/intermediate/store/weekly/forecasting/int__weekly_sales_arima_forecasts.sql
+++ models/intermediate/store/weekly/forecasting/int__weekly_sales_arima_forecasts.sql
@@ -15,10 +15,10 @@ SELECT
     p.confidence_interval_upper_bound,
     CASE WHEN p.forecast_value < 0 THEN 0 ELSE p.forecast_value END AS forecast_value_floor,
     DATE_TRUNC(CURRENT_DATE(), MONTH) AS forecast_generation_date
-FROM {{ iowa_liquor_sales.my_forecast(ref('int__weekly_sales_arima_model'), '12') }} p
-JOIN {{ ref('int__category_profile') }} c
+FROM {{ iowa_liquor_sales.my_forecast(ref('int__weekly_sales_arima_models'), '12') }} p
+JOIN {{ ref('int__category_profiles') }} c
     ON c.category = p.category
-JOIN {{ ref('int__store_profile') }} s
+JOIN {{ ref('int__store_profiles') }} s
     ON s.store_number=p.store_number
 
 )
