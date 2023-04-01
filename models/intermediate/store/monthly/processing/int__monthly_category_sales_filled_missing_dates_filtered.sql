b7b46d77738887a285bbc5dfe174182dd4c3fb57
bveber-phdata <bveber@phdata.com>
2023-03-31 21:49:44 -0500

    working python time-series forecasting using dataproc serverless

---
+++ models/intermediate/store/monthly/processing/int__monthly_category_sales_filled_missing_dates_filtered.sql
@@ -0,0 +1,12 @@
+WITH source_data AS (
+    SELECT
+    store_number,
+    category,
+    sales_month,
+    CASE WHEN total_sales IS NULL THEN 0 ELSE total_sales END AS total_sales,
+    CASE WHEN total_gallons IS NULL THEN 0 ELSE total_gallons END AS total_gallons
+    FROM {{ ref('int__monthly_category_sales_filled_missing_dates') }}
+    WHERE store_number in ('4004', '2633')
+)
+
+SELECT * FROM source_data
